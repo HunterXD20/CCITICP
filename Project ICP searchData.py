c0073abf31072f830c14aca77a66d52b69377038
HunterXD20 <58209803+HunterXD20@users.noreply.github.com>
2019-12-04 02:27:50 +0800

    Update Project ICP searchData.py

--- Project ICP searchData.py
+++ Project ICP searchData.py
@@ -1,48 +1,43 @@
 #searchData.py
 
-import os.path
+import os
 
-def main():
-    print(__file__.split('/')[-1])
-    fileName = input("Enter a OUTPUT file name for writing:")
-    searchData(fileName)
-
-def searchData(fileName):
-    inFile = open(fileName, "r")
-    recNum_input = int(input("Please enter Record Number:"))
-    recNum = inFile.readline()
-    for itemRec in inFile :
-        if  itemRec == recNum_input :
-            itemName = str(inFile.readline())
-            itemNum = int(inFile.readline())
-            category = inFile.readline()
-            quantity = int(inFile.readline())
-            weight = int(inFile.readline())
-            recipient = inFile.readline()
-            finalDes = inFile.readline()
-            delStatus = inFile.readline()
-
-            print("Item Name:", itemName, end="")
-            print("Item Number:", itemNum, end="")
-            print("Category:", category, end="")
-            print("Quantity:", quantity, end="")
-            print("Weight:", weight, end="")
-            print("Recipient:", recipient, end="")
-            print("Final Destination:", finalDes, end="")
-            print("Delivery Status:", delStatus)
-            print()
-
-           recNum = inFile.readline()
-           searchItem = input("\nDo you want to search another item record? (y/n):")
-           if searchItem == "n":
-               break
-               
+while(True):
+    itemRec_input = int(input("\nPlease enter Record Number for Advanced Searching:"))
+    itemRec_input = itemRec_input-1001
+    inFile = open("inventory.txt", "r")
+    index = itemRec_input*10
+    for itemRec in range(index) :
+        e = inFile.readline()
+    recNum1 = inFile.readline()
+    itemName1 = inFile.readline()
+    itemNum1 = inFile.readline()
+    category1 = inFile.readline()
+    quantity1 = inFile.readline()
+    weight1 = inFile.readline()
+    recipient1 = inFile.readline()
+    finalDes1 = inFile.readline()
+    delStatus1 = inFile.readline()
     inFile.close()
 
-
-main()
-
+    print("Record Number:", recNum1, end="") 
+    print("Item Name:", itemName1, end="")
+    print("Item Number:", itemNum1, end="")
+    print("Category:", category1, end="")
+    print("Quantity:", quantity1, end="")
+    print("Weight:", weight1, end="")
+    print("Recipient:", recipient1, end="")
+    print("Final Destination:", finalDes1, end="")
+    print("Delivery Status:", delStatus1, end="")
     
+
+    searchItem = input("Do you want to search another item record? (y/n):")
+    if searchItem == "n":
+        print("\nHope to see you next time ^.^")
+        break
+    
+
+
 
     
 
